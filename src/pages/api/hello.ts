e633b348217dff4db68f8a9362cf317e82fd0f36
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-10-11 16:50:16 -0300

    feat: get array of data

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,21 +1,45 @@
 import { query as q } from 'faunadb';
 import { fauna } from '../../services/fauna';
 
+interface Record {
+  ts: number;
+  data: {
+    stationId: string;
+    name: string;
+  }
+}
+interface QueryResultType {
+  data: Record[];
+}
+
 async function handler(req, res) {
 
-  await fauna.query(
-    q.Create(
-      q.Collection('records'),
-      { data: { name: 'John Doe' } }
-    )
-  )
+  // Mock de um request da estação
+  // await fauna.query(
+  //   q.Create(
+  //     q.Collection('records'),
+  //     { data: { name: 'John Doe 2', stationId: 'if-001' } }
+  //   )
+  // )
 
-  const teste = await fauna.query(
-    q.Get(
-      q.Collection('records')
+  const queryResult: QueryResultType = await fauna.query(
+    q.Map(
+      q.Paginate(
+        q.Match(q.Index('records_by_station_id'), 'if-001'),
+      ),
+      q.Lambda(x => q.Get(x))
     )
-  )
-  res.status(200).json({ ...teste, date: new Date(teste.ts / 1000) })
+  );
+
+  const docs = queryResult?.data.map(item => {
+    console.log(item)
+    return {
+      created_at: new Date(item.ts / 1000),
+      ...item.data
+    }
+  })
+
+  res.status(200).json(docs)
 }
 
 export default handler
