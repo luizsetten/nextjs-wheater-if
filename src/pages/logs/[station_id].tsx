4f654b0ac01eb00543019ec79c869e05a89a486f
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-12-08 17:23:59 -0300

    feat: add charts

---
+++ src/pages/logs/[station_id].tsx
@@ -0,0 +1,10 @@
+import { Line } from 'react-chartjs-2';
+
+function Logs() {
+  return <div> 
+    {/* <Line data={{}}/> */}
+    https://react-chartjs-2.netlify.app/examples/line-chart
+     </div>
+}
+
+export default Logs;
